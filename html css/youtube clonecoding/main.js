11a68a665607be09fd89f8d8fe8148e4c13455bb
shlee <83402978+develeep@users.noreply.github.com>
2021-10-09 00:14:37 +0900

    youtube clonecodding3
    js를 이용해 간단히 더보기, 간략히 하기

--- html css/youtube clonecoding/main.js
+++ html css/youtube clonecoding/main.js
@@ -1,6 +1,9 @@
 const toggleBtn = document.querySelector('.more');
 const timeline = document.querySelector('.timeline');
-
+const mun = document.querySelector('.mun');
+const mun2 = document.querySelector('.mun2');
 toggleBtn.addEventListener('click', () => {
   timeline.classList.toggle('active');
+  mun.classList.toggle('active');
+  mun2.classList.toggle('active');
 });
